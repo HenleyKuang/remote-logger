03d0e5d1048fb9d2dfe8bdb0bc8df6cfa946cdd6
Henley Kuang <hkuang@brightedge.com>
2019-08-14 15:44:21 -0700

    Add tests for initializing RemoteLoggerHandler

---
+++ tests/test_remote_logger_handler.py
@@ -0,0 +1,43 @@
+"""
+:author: Henley Kuang
+:since: 08/14/2019
+"""
+import pytest
+
+from remote_logger.remote_logger_handler import RemoteLoggerHandler
+from remote_logger.util.definitions import (
+    SENTRY,
+    STACKDRIVER,
+)
+from remote_logger.util.excepts import (
+    InvalidClientTypeException,
+)
+
+
+def test_initialize_sentry_logger_handler():
+    """
+    Test initialization of sentry logger handler
+    """
+    try:
+        sentry_handler = RemoteLoggerHandler(SENTRY,
+                                             dsn="https://test@sentry.io/1")
+    except Exception as e:
+        pytest.fail("Exception raised: %s" % e)
+
+
+# def test_initialize_stackdriver_logger_handler():
+#     """
+#     Test initialization of sentry logger handler
+#     """
+#     try:
+#         sentry_handler = RemoteLoggerHandler(STACKDRIVER)
+#     except Exception as e:
+#         pytest.fail("Exception raised: %s" % e)
+
+
+def test_initialize_invalid_client_type_logger_handler():
+    """
+    Test initialization of an invalid client_type
+    """
+    with pytest.raises(InvalidClientTypeException):
+        sentry_handler = RemoteLoggerHandler("this should fail")
