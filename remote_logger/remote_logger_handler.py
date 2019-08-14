dbc4274dcb2eba93b36a8e42c7ac70a782e361be
Henley Kuang <hkuang@brightedge.com>
2019-08-14 15:42:34 -0700

    Add InvalidClientTypeException and raise if client_type does not have a corresponding LoggerClient

--- remote_logger/remote_logger_handler.py
+++ remote_logger/remote_logger_handler.py
@@ -7,14 +7,23 @@ from logging import StreamHandler
 
 from remote_logger.clients.sentry_logger_client import SentryLoggerClient
 from remote_logger.util.definitions import SENTRY
+from remote_logger.util.excepts import InvalidClientTypeException
+
+CLIENT_TYPE_LOGGER_CLIENT = {
+    SENTRY: SentryLoggerClient,
+}
 
 
 class RemoteLoggerHandler(StreamHandler):
 
     def __init__(self, client_type, **kwargs):
         StreamHandler.__init__(self)
-        if client_type == SENTRY:
-            self._client = SentryLoggerClient(**kwargs)
+        try:
+            LoggerClient = CLIENT_TYPE_LOGGER_CLIENT[client_type]
+        except KeyError:
+            raise InvalidClientTypeException(
+                "No LoggerClient for client_type: %s" % client_type)
+        self._client = LoggerClient(**kwargs)
 
     def emit(self, record):
         msg = self.format(record)
---
+++ remote_logger/util/excepts.py
@@ -0,0 +1,8 @@
+"""
+:author: Henley Kuang
+:since: 08/14/2019
+"""
+
+
+class InvalidClientTypeException(Exception):
+    pass
