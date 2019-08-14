479a33e302b94db57c609cf876bdc4abcdbd4517
Henley Kuang <hkuang@brightedge.com>
2019-08-14 15:53:49 -0700

    Export SENTRY and STACKDRIVER constants in parent __init__.py

--- remote_logger/__init__.py
+++ remote_logger/__init__.py
@@ -1,5 +1,11 @@
 from remote_logger.remote_logger_handler import RemoteLoggerHandler
+from remote_logger.util.definitions import (
+    SENTRY,
+    STACKDRIVER,
+)
 
 __all__ = (
     "RemoteLoggerHandler",
+    "SENRTY",
+    "STACKDRIVER",
 )
